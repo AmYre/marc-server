20829c97591c6338c3306a64abbc3710148b7c66
AmirDev <benhammouda.amir@gmail.com>
2023-03-19 22:41:15 +0100

    re installing vision

--- dist/static/index-cf8d3806.esm.2eab7594.js
+++
@@ -1,1 +0,0 @@
-import{a as t,D as m}from"./index-989e9e04.esm.c756ad08.js";import"./sanity.9c9f6994.js";import"./index.ce353d24.js";export{t as DocumentPaneProvider,m as default};
---
+++ dist/static/index-cf8d3806.esm.7ddcb997.js
@@ -0,0 +1,1 @@
+import{a as t,D as m}from"./index-989e9e04.esm.9d374dda.js";import"./sanity.74416068.js";import"./index.0900f6a6.js";export{t as DocumentPaneProvider,m as default};
